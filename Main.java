bb98363620d766330d8356168227682b59c2c4fe
Matthew Tujague <81255678+Flaccite@users.noreply.github.com>
2022-04-28 17:35:24 +0000

    merge

--- Main.java
+++ Main.java
@@ -1,7 +1,36 @@
+import java.io.*;
+import java.util.Scanner;
+
 public class Main {
-	public static void main(String[] args) {
-		CheckersGame game = new CheckersGame();
+	public static void main(String[] args) throws IOException {
 		
-		game.gameloop();
+		Scanner input = new Scanner(System.in);
+		String decision;
+		
+		do {
+			System.out.print("1: New Game\n2: Saved Game\n>>> ");
+			decision = input.nextLine();
+			System.out.println();
+			System.out.println();
+			CheckersGame.clearConsole();
+			
+		} while(!decision.equals("1") && !decision.equals("2"));
+
+		String path;
+		if(decision.equals("1")) {
+			CheckersGame game = new CheckersGame();
+			path = game.gameloop();
+			
+		} else {
+			path = GameSave.getPath();
+			GameSave saved = GameSave.loadGame(path);
+			CheckersGame game = new CheckersGame(saved.board, saved.curTurn, path);
+			game.gameloop();
+		}
+
+		File deleteable = new File("saved/" + path + ".ser"); //When a game is won/over, delete it
+		deleteable.delete();
+		
+		input.close();
 	}
 }

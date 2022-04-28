bc42da155b8a39b8c260dc3e27509e298159b18f
Matthew Tujague <81255678+Flaccite@users.noreply.github.com>
2022-04-28 17:38:10 +0000

    merging

--- CheckersGame.java
+++ CheckersGame.java
@@ -1,18 +1,40 @@
 import java.util.Scanner;
 import java.lang.Character;
+import java.io.*;
+import java.nio.file.Files;
 
 public class CheckersGame {
-	Board board;
+	// public's
+	public static final String[] players = new String[] {"Black", "White"};
+	// private's
+	private int turnCount;
+	private boolean saving = true; //manual toggle
+	private String path;
 	
+	Board board;
 	public CheckersGame() {
 		this.board = new Board();
+		this.turnCount = 0;
+		this.path = null;
 	}
 
-	public void gameloop() {
-		String[] players = new String[] {"Black", "White"};
+	public CheckersGame(Board board, String path) {
+		this.board = board;
+		this.turnCount = 0;
+		this.path = path;
+	}
+
+	public CheckersGame(Board board, int turnCount, String path)  {
+		this.board = board;
+		this.turnCount = turnCount;
+		this.path = path;
+	}
+
+	public String gameloop() throws IOException { //Returns the path of the game to delete out of scope
+		Scanner input = new Scanner(System.in, "UTF-8");
+		
 		String currentPlayer;
 		
-		int turnCount = 0;
 		int x, y, dx, dy;
 		int[] coordinates;
 
@@ -20,31 +42,38 @@ public class CheckersGame {
 		final int STALEMATEDELIMITER = 40;
 
 		
+		if(path == null) {
+			System.out.print("Save game as:\n>>> ");
+			path = input.nextLine(); //TODO add redundancy
+			System.out.println();
+		}
+		
 		while(true) {
 			currentPlayer = players[turnCount % 2];
-			
-			if(!board.canMove(currentPlayer.charAt(0))) {
-				clearConsole();
-				System.out.println(board);
-				System.out.println("Game Over\n" + currentPlayer + " is unable to make any moves: STALEMATE");
-				return;
 				
-			} else if((turnsSinceLastJump / 2) == STALEMATEDELIMITER) { // div2 as each player should have moved 40 times
+			if((turnsSinceLastJump / 2) == STALEMATEDELIMITER) { // div2 as each player should have moved 40 times
 				clearConsole();
 				System.out.println(board);
 				System.out.println("Game Over\n40 moves have elapsed since a checker was taken: DRAW");
-				return;
+				return path;
 				
 			} else if(board.blackCount() == 0) {
 				clearConsole();
 				System.out.println(board);
 				System.out.println("Black has exausted its units: WHITE WINS");
-				return;
+				return path;
 				
 			} else if(board.whiteCount() == 0) {
 				clearConsole();
 				System.out.println(board);
 				System.out.println("White has exausted its units: BLACK WINS");
+			
+			} else if(!board.canMove(currentPlayer.charAt(0))) {
+				clearConsole();
+				System.out.println(board);
+				System.out.println("Game Over\n" + currentPlayer + " is unable to make any moves: STALEMATE");
+				return path;
+				
 			} else {
 				coordinates = getProperCoordinates(currentPlayer);
 				x = coordinates[0];
@@ -56,14 +85,15 @@ public class CheckersGame {
 				//At this point we have x, y, dx, dy; we also know they are valid moves
 	
 				if(Math.abs(dx-x) == 2 && Math.abs(dy-y) == 2) { //We know that the move is valid and two spaces away, we are jumping
+					turnsSinceLastJump = 0;
+					
 					int[] middlePosition = board.getIntermediaryPosition(x, y, dx, dy);
-	
 					board.move(new int[] {x, y}, middlePosition); //Short stopping at our middle position to kill enemy
 					board.move(middlePosition, new int[] {dx, dy}); //from short stop to final position
 
+					String[] jumpingNames = new String[] {"FILLER", "DOUBLE", "TRIPLE", "QUADRUPLE", "QUINTUPLE", "SEXTUPLE", "SEPTUPLE", "OCTUPLE", "N-TUPLE", "DOU-DECUPLE"};
 					boolean jumping = true;
 					int count = 0;
-					String[] jumpingNames = new String[] {"FILLER", "DOUBLE", "TRIPLE", "QUADRUPLE", "QUINTUPLE", "SEXTUPLE", "SEPTUPLE", "OCTUPLE", "N-TUPLE", "DOU-DECUPLE"};
 					
 					while(jumping) { 
 						//dx dy are our new starting coordinates after initial move above
@@ -85,6 +115,8 @@ public class CheckersGame {
 								
 							} while(!board.isJumpableMove(x, y, dx, dy));
 								//x, y, dx, dy will be a jumpable move at this point in time
+								turnsSinceLastJump = 0;
+							
 								middlePosition = board.getIntermediaryPosition(x, y, dx, dy);
 								board.move(new int[] {x, y}, middlePosition); //Short stopping at our middle position to kill enemy
 								board.move(middlePosition, new int[] {dx, dy}); //from short stop to final position
@@ -102,9 +134,13 @@ public class CheckersGame {
 				}
 				
 				turnCount++;
+				if(saving) {
+					new GameSave(board, turnCount).saveToFile(path);
+				}
 			}
 			
 		}
+		
 	}
 
 	public int[] getProperCoordinates(String currentPlayer) {
@@ -197,8 +233,9 @@ public class CheckersGame {
 		return (x >= 0 && x <= 7) && (y >= 0 && y <=7);
 	}
 	
-	public void clearConsole() {
+	public static void clearConsole() {
 		System.out.print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
 		System.out.flush();
 	}
+	
 }
